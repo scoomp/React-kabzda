7ba30de4e52617319ade535b197f4bbaba08b5d3
scoomp <moshnyakov.alexei@yandex.ru>
2023-06-12 15:15:08 +0300

    Change README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,32 @@
 import React from 'react';
 import './App.css';
-import Accordion from "./componets/Accordion/Accordion";
-import {Rating} from "./componets/Accordion/Rating";
-
-function hello() {
-    alert('HELLO IT_KAMASUTRA')
-}
- hello();
-
-
+import {Rating} from "./components/Rating/Rating";
+import {Accordion} from "./components/Accordion/Accordion";
+import {OnOff} from "./components/OnOff/OnOff";
+import {UncontrolledOnOff} from "./components/OnOff/UncontrolledOnOff";
+import {UncontrolledAccordion} from "./components/Accordion/UncontrolledAccordion";
+import {UncontrolledRating} from "./components/Rating/UncontrolledRating";
 
 function App() {
-    console.log("App rendering")
     return (
-        <div>
-            <PageTitle title={"This is APP component"} />
-            <PageTitle title={"My friends"} />
-            Article 1
-            <Rating value={3} />
-            <Accordion titleValue={"Menu"} collapsed={true}/>
-            <Accordion titleValue={"Users"} collapsed={false}/>
-            Article 2
+        <div className="App">
             <Rating value={0}/>
             <Rating value={1}/>
             <Rating value={2}/>
             <Rating value={3}/>
             <Rating value={4}/>
             <Rating value={5}/>
+            <UncontrolledRating/>
+            <hr/>
+            <Accordion title={'Menu'} collapsed={true}/>
+            <Accordion title={'Menu'} collapsed={false}/>
+            <UncontrolledAccordion title={'Menu'}/>
+            <hr/>
+            <OnOff isActive={true}/>
+            <OnOff isActive={false}/>
+            <UncontrolledOnOff />
         </div>
     );
-}
-
-type PagePropsType = {
-    title: string
-}
-
-function PageTitle(props:PagePropsType) {
-    console.log("PageTitle rendering")
-    return <h1>{props.title}</h1>
 }
 
 export default App;
